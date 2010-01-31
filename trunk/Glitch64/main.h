14b25413642eb3a9b8894942ffa9079f1bd64722
gonetz@ngs.ru <gonetz@ngs.ru@74557250-ff2b-11dd-9fdb-3debd5008f28>
2010-01-31 13:51:31 +0000

    Implemented gamma correction support. Windows only.

--- trunk/Glitch64/main.h
+++ trunk/Glitch64/main.h
@@ -132,7 +132,7 @@ FX_ENTRY void FX_CALL grConfigWrapperExt(FxI32, FxI32, FxBool, FxBool);
 FX_ENTRY GrScreenResolution_t FX_CALL grWrapperFullScreenResolutionExt(FxU32*, FxU32*);
 FX_ENTRY char ** FX_CALL grQueryResolutionsExt(FxI32*);
 FX_ENTRY FxBool FX_CALL grKeyPressedExt(FxU32 key);
-
+FX_ENTRY void FX_CALL grGetGammaTableExt(FxU32, FxU32*, FxU32*, FxU32*);
 
 int getFullScreenWidth();
 int getFullScreenHeight();
